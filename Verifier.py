78d431456fdfbb5297fb72e4c335da4dfea29620
Jarvis Chung (RD-TW) <germmail@gmail.com>
2012-07-24 15:40:56 +0800

    Updated Xml verifier. Added decrypt function.

--- Verifier.py
+++ Verifier.py
@@ -16,6 +16,7 @@ import ConfigParser
 from xml.etree.ElementTree import Element
 from xml.etree.ElementTree import ElementTree
 from xml.etree.ElementTree import SubElement
+import commands
 
 class Verifier(object):
     """docstring for Verifier"""
@@ -28,8 +29,8 @@ class Verifier(object):
         """docstring for verify"""
         report = []
         #report += IniVerifier(self.config_path, self.testcase).verify()
-        #report += XmlVerifier(self.config_path, self.testcase).verify()
-	report += DBVerifier(self.config_path, self.testcase).verify()
+        report += XmlVerifier(self.config_path, self.testcase).verify()
+	#report += DBVerifier(self.config_path, self.testcase).verify()
 	#report += LOGVerifier(self.config_path, self.testcase).verify()
         return report
 
@@ -91,14 +92,14 @@ class XmlVerifier:
             result['file'] = os.path.basename(xml['path'])
             result['type'] = 'xml'
             print 'checking %s' % result['file']
-            xml_path = os.path.join(os.getcwd(), self.testcase, xml['path'])
+	    xml_path = os.path.join(os.getcwd(), self.testcase, xml['path'])
 	    parser = ElementTree()
             tree = parser.parse(xml_path)
 	    itera =  tree.getiterator()
 	    for option in xml['data']:
 	    	parser_name = option[0]
 	   	parser_value = option[1]
-
+		#print parser_name + ' | ' + parser_value
 	    	for element in itera:
 			is_pass = False
 			has_option = False
@@ -110,9 +111,15 @@ class XmlVerifier:
 			elif len(element.items())<=1: #string
 				if str(element.get('name')).lower() in parser_name:
 					has_option = True
-					if parser_value in element.text:
+					xml_parser_value = element.text
+					if self.isDecrypt(parser_name.upper()):
+						#print xml_parser_value
+						cmd = "java -jar DebugToolAES.jar " + xml_parser_value
+						xml_parser_value = commands.getoutput(cmd)
+						#print 'Decrypt content:' + str(xml_parser_value)
+					if parser_value in xml_parser_value:
 						is_pass = True
-						print '**Find:', parser_name
+						#print '**Find:', parser_name + ' ' + str(element.text)
 					#print "#string name:", element.get('name')
 					#print "#string value:", element.text
 				else:
@@ -120,9 +127,13 @@ class XmlVerifier:
 			else: #int, boolean
 				if str(element.get('name')).lower() in parser_name:
 					has_option = True
+					if self.isDecrypt(parser_name.upper()):
+						cmd = "java -jar DebugToolAES.jar " + xml_parser_value
+						xml_parser_value = commands.getoutput(cmd)
+						#print 'Decrypt content:' + parser_value
 					if parser_value in element.get('value'):
 						is_pass = True
-						print '**Find:', parser_name
+						#print '**Find:', parser_name + ' ' + str(element.text)
 					#print "#value type:"
 					#print '#name:', element.get('name')
 					#print element.attrib
@@ -133,8 +144,17 @@ class XmlVerifier:
 			    is_pass = False
 			    has_option = False
 	
-	report.append(result)
+	    report.append(result)
         return report
+
+    def isDecrypt(self, name):
+	def_type = ['BIZ_TYPE', 'EOLKEY', 'EXPIREDATE', 'AUTORENEW', 'SUPERKEY', 'UID', 'LICENSE_STATUS', 'ENCRYPT_PASSWORD']
+	try:
+	    result = def_type.index(name)
+	    #print 'isDecrypt:' + name
+	    return True
+	except ValueError:
+	    return False
     
     def _expected_data(self):
         parser = ConfigParser.ConfigParser()
@@ -174,7 +194,7 @@ class DBVerifier:
 	    if len(data) > 0:
 	        result['result'].append((db_col, db_value, True))
 
-	report.append(result)
+	    report.append(result)
 	return report
 
     def _expected_data(self):
@@ -196,6 +216,7 @@ class LOGVerifier:
             result['type'] = 'log'
             print 'checking %s' % result['file']
 	    parser_filter = ''
+	    log_path = os.path.join(os.getcwd(), self.testcase, log['path'])
 
 	    for option in log['data']:
 	    	if option[0] in 'filter':
@@ -208,7 +229,7 @@ class LOGVerifier:
 	    	    parser_value = option[1]
 	    	#print parser_name + ' ' + parser_value
 		
-		f = open("TestCase0/" + log['path'])
+		f = open(log_path)
 	    	buf = f.readline()
 		is_find = False
 	   	while buf:
@@ -236,7 +257,7 @@ class LOGVerifier:
 			result['result'].append((parser_name, parser_value, False))
 	    	f.close()
 
-	report.append(result)
+	    report.append(result)
 	return report
 
     def _expected_data(self):
