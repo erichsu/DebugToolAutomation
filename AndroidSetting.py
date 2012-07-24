2a7d58f9958f4bf0ff0633afe514452effbc0c38
Eric Hsu <eric_hsu@trend.com.tw>
2012-07-24 17:32:56 +0800

    Refine AndroidSetting code

--- AndroidSetting.py
+++ AndroidSetting.py
@@ -1,56 +1,63 @@
-'''
-This is a subfunction of automation tool for adb command executing
-'''
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+AndroidSetting.py
+
+Created by Eric Hsu (RD-TW) on 2012-07-11.
+Copyright (c) 2012. All rights reserved.
+"""
 
 import subprocess
 
 class AndroidSetting(object):
-    '''
-    classdocs
-    '''
-
     def __init__(self, config=None):
         self.config = config
         self.options = ['battery', 'wifi', 'date', 'sms']
         
     def setup(self):
-        #a None value indicates that the process hasn't terminated yet.
-        status = None
-        
-        '''self._unlock_screen()'''
-        
-        for key in self.options:
-            if key in self.config and self.config[key] != '':
-                method = getattr(self, '_'+key)
-                status = method(self.config[key])
-
-        return status
+        for option in self.options:
+            if option in self.config and self.config[option] != '':
+                method = getattr(self, '_' + option)
+                status = method(self.config[option])
     
     def _unlock_screen(self):
-        #cmd = '%(adb)s shell input keyevent 82' % env
-        #proc = subprocess.Popen(cmd.split())
-        #return proc.wait()
-        return 0
+        cmd = '%(adb)s shell input keyevent 82' % self.config
+        proc = subprocess.Popen(cmd.split())
+        return proc.wait()
     
     def _date(self, datetime=''):
         print 'Setup date time'
         cmd = '%(adb)s wait-for-device shell date -s %(date)s' % self.config
-        proc = subprocess.Popen(cmd.split())
-        proc.wait()
+        # proc = subprocess.Popen(cmd.split())
+        # proc.wait()
+        print cmd
     
     def _battery(self, volume='50'):
         print 'Setup battery'
-        return 0
+        cmd = '%(adb)s wait-for-device emu power ac on' % self.config
+        # proc = subprocess.Popen(cmd.split())
+        # proc.wait()
+        print cmd
+        
+        cmd = '%(adb)s wait-for-device emu power status discharging' % self.config
+        # proc = subprocess.Popen(cmd.split())
+        # proc.wait()
+        print cmd
+        
+        cmd = '%(adb)s wait-for-device emu power %s' % self.config
+        # proc = subprocess.Popen(cmd.split())
+        # proc.wait()
+        print cmd
     
     def _sms(self, msg=''):
         print 'Setup sms'
-        return 0
+        # ./adb emu sms send 886911123456 <text message>
     
     def _wifi(self, on_off='off'):
         print 'Setup WIFI'
-        return 0
+        # ./adb emu gsm data <on|off>
         
 if __name__ == "__main__":
     android_setting = AndroidSetting({'battery':'25', 'wifi':'off', 'date':'20101010', 'adb':'/Users/eric_hsu/Downloads/android-sdk-macosx/platform-tools/adb'})
-    print 'result code: %s', android_setting.setup()
+    android_setting.setup()
     
