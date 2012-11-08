e19d649e08d112a2e792afb89d6855b2c8c656cb
Eric Hsu <eric_hsu@trend.com.tw>
2012-11-08 16:15:06 +0800

    Apply TMMS 3.0 for demo

--- TestCase2/Case2.py
+++ TestCase2/Case2.py
@@ -1,5 +1,5 @@
 #Case 2
-#Launch TMMS and verfiy pattern version 
+#Launch TMMS 3.0 and verfiy pattern version 
 
 import time
 import datetime
@@ -11,43 +11,31 @@ from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 # Connects to the current device, returning a MonkeyDevice object
 device = MonkeyRunner.waitForConnection()
 
-#device.installPackage(r"d:\tmms\scripts\TMMS_HTC.apk")
-
-#Trigger TMMS update
-#settings_package = 'com.trendmicro.tmmspersonal.HTC'
-settings_package = 'com.trendmicro.tmmssuite.consumer'
-settings_activity = 'com.trendmicro.tmmssuite.antimalware.ui.TmmsUpdateActivity'
-settings_runComponent = settings_package + '/' + settings_activity
-device.startActivity(component=settings_runComponent)
-MonkeyRunner.sleep(8)
 
 #Open TMMS UI of Antimalware
-settings_activity = 'com.trendmicro.tmmssuite.antimalware.ui.AntimalwareTab'
+settings_package = 'com.trendmicro.tmmssuite.consumer'
+settings_activity = 'com.trendmicro.tmmssuite.consumer.scanner.threat.ThreatScannerMain'
 settings_runComponent = settings_package + '/' + settings_activity
-#device.startActivity(component='com.trendmicro.tmmspersonal.HTC/com.trendmicro.tmmssuite.consumer.login.ui.Login')
 device.startActivity(component=settings_runComponent)
-MonkeyRunner.sleep(2)
-
-# Press the Menu button
-#device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
+MonkeyRunner.sleep(5)
 
 #Press the Update tab
-MonkeyRunner.sleep(2)
-device.touch(250,180,"DOWN_AND_UP") 
-MonkeyRunner.sleep(2)
+device.touch(250,410,"DOWN_AND_UP") 
+MonkeyRunner.sleep(5)
 
+#Trigger TMMS update by click icon
+device.touch(430,510,"DOWN_AND_UP") 
+MonkeyRunner.sleep(20)
+device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 
-# now=strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+now=strftime("%Y-%m-%d-%H-%M-%S", gmtime())
 
 
 # Takes a screenshot
 result = device.takeSnapshot()
 
 # Writes the screenshot to a file
-# result.writeToFile(r"shot-" + now +r".png",'png')
-result.writeToFile(r"screenshot.png",'png')
+result.writeToFile(r"shot-" + now +r".png",'png')
 
 #Clean up UI
-for x in range(10):
-    device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
-    MonkeyRunner.sleep(1)
+#device.press('KEYCODE_HOME', MonkeyDevice.DOWN_AND_UP)
